f0c9741767386b4121723ffe43e0bdd4b9451ec0
Denis Karlinsky <keeeeeves@gmail.com>
2023-03-04 23:35:56 +0200

    updated navigation by products

--- client/src/components/ui/ProductNavigation/ProductNavigation.tsx
+++ client/src/components/ui/ProductNavigation/ProductNavigation.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/react'
+import { Button, Tooltip } from '@chakra-ui/react'
 import { FC } from 'react'
 
 import styles from './ProductNavigation.module.scss'
@@ -9,16 +9,21 @@ import Link from 'next/link'
 import { useProductNavigation } from '@/hooks/useProductNavigation'
 
 const ProductNavigation: FC<{ productId: number }> = ({ productId }) => {
-  const { slugPrev, slugNext } = useProductNavigation(productId)
+  const { slugPrev, slugNext, nameNext, namePrev } =
+    useProductNavigation(productId)
 
   return (
     <nav className={styles.nav}>
       <Link href={`/${LINKS.product}/${slugPrev}`}>
-        <ChevronLeftIcon fontSize={30} />
+        <Tooltip label={namePrev}>
+          <ChevronLeftIcon fontSize={30} />
+        </Tooltip>
       </Link>
       <span className='cursor-default'>Navigate</span>
       <Link href={`/${LINKS.product}/${slugNext}`}>
-        <ChevronRightIcon fontSize={30} />
+        <Tooltip label={nameNext}>
+          <ChevronRightIcon fontSize={30} />
+        </Tooltip>
       </Link>
     </nav>
   )
--- client/src/hooks/useProductNavigation.tsx
+++ client/src/hooks/useProductNavigation.tsx
@@ -1,5 +1,7 @@
 import { useProducts } from './useProducts'
 
+import { IProduct, IProductDetails } from '@/types/Product'
+
 export const useProductNavigation = (productId: number) => {
   const { products } = useProducts()
 
@@ -13,10 +15,24 @@ export const useProductNavigation = (productId: number) => {
       ? String(products[0].slug)
       : String(products.find(i => i.id === productId + 1)?.slug)
 
+  const prevName = () =>
+    productId > 1
+      ? String(products.find(i => i.id === productId - 1)?.name)
+      : String(products[products.length - 1].name)
+
+  const nextName = () =>
+    products.some(item => item.id === productId + 1)
+      ? String(products[0].name)
+      : String(products.find(i => i.id === productId + 1)?.name)
+
   const prev = prevSlug()
   const next = nextSlug()
+  const namePrev = prevName()
+  const nameNext = nextName()
   return {
     slugPrev: prev,
-    slugNext: next
+    slugNext: next,
+    nameNext,
+    namePrev
   }
 }
